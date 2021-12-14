2f97b5fba2fda1c484d059e8e44f1ea3bbbcc2c0
vandueniecky <vandu.niecky14@gmail.com>
2021-12-14 11:37:15 +0700

    My Website

---
+++ script.js
@@ -0,0 +1,17 @@
+// click trigger
+$('.scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+});
+
+// GSAP 
+gsap.registerPlugin(TextPlugin);
+gsap.from('nav', {duration:1.2, y: '-100%', opacity:0, ease:'bounce'});
+gsap.to('.lead', {duration:2, delay:2.2, text: 'Graphic Dasigner | Web Developer' });
+gsap.to('.display-4', {duration:1, delay:1.5, text: 'Van Due Niecky'});
+gsap.from('.welcome-section img', {duration:1.5, rotateY: 360, opacity:0});
+
+// AOS JS
+AOS.init({
+    once: true, // Animasi dijalankan satu kali saja. Kecuali di Load.
+    offset: 100, // Offset Animasi berjalan sebelum 100px
+});
