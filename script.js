911f3485d03650f717aea32eaad65911798fa249
vandueniecky <vandu.niecky14@gmail.com>
2022-07-21 17:16:48 +0700

    Update 0.21.07.22

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $('.scroll-trigger').click(function() {
 // GSAP 
 gsap.registerPlugin(TextPlugin);
 gsap.from('nav', {duration:1.2, y: '-100%', opacity:0, ease:'bounce', delay:2});
-gsap.to('.lead', {duration:2, delay:4.2, text: 'Graphic Design | Web Developer' });
+gsap.to('.lead', {duration:2, delay:4.2, text: 'Graphic Design | Front-End Developer' });
 gsap.to('.display-4', {duration:1, delay:3.5, text: 'Van Due Niecky'});
 gsap.from('.welcome-section img', {duration:1.5, rotateY: 360, opacity:0, delay:2.1});
 gsap.from('.project1 img', {duration:1.5, rotateY: 360, opacity:0, delay:2});
